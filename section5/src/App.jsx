f842076c81dfc6f5e97cb0e348c44072a4a91ec9
김보미 <kimbomi2172@naver.com>
2024-04-25 17:14:09 +0900

    state로 상태 관리하기

--- section5/src/App.jsx
+++ section5/src/App.jsx
@@ -1,23 +1,55 @@
-import Button from './components/Button'
-import Header from './components/Header'
+// import Button from './components/Button'
+// import Header from './components/Header'
 
-const buttonProps = {
-  text: '메일',
-  color: 'red',
-  a: 1,
-  b: 2,
-  c: 3,
-}
+import { useState } from 'react'
+
+// const buttonProps = {
+//   text: '메일',
+//   color: 'red',
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <Button {...buttonProps} />
+//       <Button text={'카페'} />
+//       <Button text={'블로그'}>
+//         <div>자식요소</div>
+//         <Header />
+//       </Button>
+//     </>
+//   )
+// }
+
+// export default App
 
 function App() {
+  const [state, setState] = useState(0)
+  const [light, setLight] = useState('OFF')
+
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={'카페'} />
-      <Button text={'블로그'}>
-        <div>자식요소</div>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === 'ON' ? 'OFF' : 'ON')
+          }}
+        >
+          {light === 'ON' ? '끄기' : '켜기'}
+        </button>
+      </div>
+      <h1>{state}</h1>
+      <button
+        onClick={() => {
+          setState(state + 1)
+        }}
+      >
+        +
+      </button>
     </>
   )
 }
