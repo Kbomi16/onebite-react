b3688a6fd93c333a185a1ae72f9c9fc4ca34d4ca
김보미 <kimbomi2172@naver.com>
2024-04-25 20:41:59 +0900

    state를 props로 전달하기

--- section5/src/App.jsx
+++ section5/src/App.jsx
@@ -1,55 +1,12 @@
-// import Button from './components/Button'
-// import Header from './components/Header'
-
-import { useState } from 'react'
-
-// const buttonProps = {
-//   text: '메일',
-//   color: 'red',
-//   a: 1,
-//   b: 2,
-//   c: 3,
-// }
-
-// function App() {
-//   return (
-//     <>
-//       <Button {...buttonProps} />
-//       <Button text={'카페'} />
-//       <Button text={'블로그'}>
-//         <div>자식요소</div>
-//         <Header />
-//       </Button>
-//     </>
-//   )
-// }
-
-// export default App
+/* eslint-disable react/prop-types */
+import Bulb from './components/Bulb'
+import Counter from './components/Counter'
 
 function App() {
-  const [state, setState] = useState(0)
-  const [light, setLight] = useState('OFF')
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === 'ON' ? 'OFF' : 'ON')
-          }}
-        >
-          {light === 'ON' ? '끄기' : '켜기'}
-        </button>
-      </div>
-      <h1>{state}</h1>
-      <button
-        onClick={() => {
-          setState(state + 1)
-        }}
-      >
-        +
-      </button>
+      <Bulb />
+      <Counter />
     </>
   )
 }
