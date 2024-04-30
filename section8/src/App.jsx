b59bc02529ded5a4e05bd0323687347a4e579131
김보미 <kimbomi2172@naver.com>
2024-04-30 13:00:07 +0900

    ✨ feat: UI 구현

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
 
 function App() {
   return (
-    <div>
-      
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
