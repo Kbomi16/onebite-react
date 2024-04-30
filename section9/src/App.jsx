f9937d9cadb142bccfece746dedcf7cccfc7c5d1
김보미 <kimbomi2172@naver.com>
2024-04-30 19:43:41 +0900

    ✨ feat: 투두리스트 업그레이드(useReducer)

--- section9/src/App.jsx
+++ section9/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useReducer, useRef, useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
@@ -26,47 +26,58 @@ const mockData = [
   },
 ]
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state]
+    case 'UPDATE':
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      )
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId)
+    default:
+      return state
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData)
+  const [todos, dispatch] = useReducer(reducer, mockData)
+
   const idRef = useRef(3)
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    }
-
-    setTodos([newTodo, ...todos])
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    })
   }
 
   const onUpdate = (targetId) => {
-    // todos state 값들 중에 targetId와 일치하는 id를 갖는 투두 아이템의 isDone변경
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === targetId) {
-          return {
-            ...todo,
-            isDone: !todo.isDone,
-          }
-        }
-        return todo
-      })
-    )
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId,
+    })
   }
 
   const onDelete = (targetId) => {
-    // todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId))
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    })
   }
 
   return (
     <div className="App">
-      <Exam />
-      {/* <Header />
+      {/* <Exam /> */}
+      <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   )
 }
