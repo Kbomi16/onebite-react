74559fcce0e6afda4d4fa1ff4e564edd5269cdcc
김보미 <kimbomi2172@naver.com>
2024-04-30 19:09:52 +0900

    🎉 프로젝트 초기 세팅

---
+++ section9/src/components/List.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import './List.css'
+import TodoItem from './TodoItem'
+
+function List({ todos, onUpdate, onDelete }) {
+  const [search, setSearch] = useState('')
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    )
+  }
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input placeholder="검색어를 입력하세요." value={search} onChange={onChangeSearch} />
+      <div className="todos_wrapper">
+        {getFilteredData().map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default List
