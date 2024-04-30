d92340ef4a5d6cffab64cb6556ab2fd09a92c3b8
김보미 <kimbomi2172@naver.com>
2024-04-30 14:16:51 +0900

    ✨ feat: 기능 구현 준비

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
 import './App.css'
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: '',
+    date: new Date().getTime(),
+  },
+]
+
 function App() {
+  const [todos, setTodos] = useState([])
+
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Editor />
       <List />
